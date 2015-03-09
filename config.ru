946fb861943fe6fc9f9be030f5f4ae4c7d65d37d
David Cristofaro <git@dtcristo.com>
2015-03-09 21:57:20 +1100

    Add basic auth with .env, use puma instead of thin

--- config.ru
+++ config.ru
@@ -1,9 +1,13 @@
+require 'dotenv'
 require 'faye'
-require_relative 'hello'
+require_relative 'message_extension'
 require_relative 'sweet_talk'
 
+# Load environment variables
+Dotenv.load
+
 use Faye::RackAdapter, mount: '/faye', timeout: 25 do |faye|
-  faye.add_extension(Hello.new)
+  faye.add_extension(MessageExtension.new)
 end
 
 run Sinatra::Application
--- sweet_talk.rb
+++ sweet_talk.rb
@@ -6,6 +6,11 @@ require 'json'
 # Store all messages here
 $messages = []
 
+use Rack::Auth::Basic do |username, password|
+  username == ENV['SWEET_TALK_USERNAME'] &&
+    password == ENV['SWEET_TALK_PASSWORD']
+end
+
 # Handle conversion of CoffeeScript to JavaScript
 get '/index.js' do
   coffee :'../public/index'
@@ -17,11 +22,11 @@ end
 
 def process_message(message)
   # Easter egg
-  if message['text'].include? '<script>'
-    message['text'] = 'Nice try!'
-  end
+  message['text'] = 'Nice try!' if message['text'].include? '<script>'
+
   # Strip unwanted params and escape HTML
-  message = {'name' => CGI::escapeHTML(message['name']), 'text' => CGI::escapeHTML(message['text'])}
+  message = { 'name' => CGI.escapeHTML(message['name']),
+              'text' => CGI.escapeHTML(message['text']) }
   # Add timestamp
   message['time'] = Time.now.to_i
   # Save the message
@@ -30,7 +35,7 @@ end
 
 def save_message(message)
   # Log the message
-  puts "Saved: " + message.inspect
+  puts 'Saved: ' + message.inspect
   # Save the message into the array
   $messages << message
 end
